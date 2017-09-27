b9f8203c4001afb0b19aa529932825fdc34ad8e2
Limaep <limae.phuah@gmail.com>
2017-09-27 12:16:05 +0300

    Added picture

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'haml-rails'
 gem 'bootstrap'
 gem 'jquery-rails'
 gem 'simple_form'
+gem "paperclip"
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
+                    default_url: "/images/users/avatars/:style.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
