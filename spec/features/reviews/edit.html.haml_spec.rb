139b4ac90704bdc4bb9ae5e210241515f28f06ba
Luke Marco <luke.am89@hotmail.co.uk>
2017-09-29 16:11:23 +0300

    created review model and testing

---
+++ spec/features/reviews/edit.html.haml_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "reviews/edit", type: :view do
+  before(:each) do
+    @review = create(:review)
+  end
+
+  it "renders the edit review form" do
+  end
+end
---
+++ spec/features/reviews/new.html.haml_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "reviews/new", type: :view do
+  before(:each) do
+    @review = create(:review)
+  end
+
+  it "renders new review form" do
+  end
+end
