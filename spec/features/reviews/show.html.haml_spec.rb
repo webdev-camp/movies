ddb6127a4ab2ba54b5dd1750b1cc0d2db491f68a
Luke Marco <luke.am89@hotmail.co.uk>
2017-10-02 15:32:42 +0300

    Added testing for reviews

--- spec/features/reviews/show.html.haml_spec.rb
+++ spec/features/reviews/show.html.haml_spec.rb
@@ -1,29 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "reviews/show" do
+RSpec.describe "reviews/edit" do
   before(:each) do
-    @review = create(:review)
-    visit review_path(@review)
   end
-
-  it "displays the review plot score" do
-    expect(page).to have_content("Plot score: #{@review.plot_score}")
-  end
-
-  it "displays the review plot comment" do
-    expect(page).to have_content("Plot comment: #{@review.plot_comment}")
-  end
-
-  it "displays the review acting score" do
-    expect(page).to have_content("Acting score: #{@review.acting_score}")
-  end
-
-  it "displays the review acting comment" do
-    expect(page).to have_content("Acting comment: #{@review.acting_comment}")
-  end
-
-  it "displays the review summary" do
-    expect(page).to have_content("Summary: #{@review.summary}")
-  end
-
 end
