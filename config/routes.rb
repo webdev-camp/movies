b37cad743e73d2a39e6531bedd635d3ed52e5842
Luke Marco <luke.am89@hotmail.co.uk>
2017-10-02 15:28:00 +0300

    Created movie controller outside of admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   get 'home/movie'
-
+  resources :movies, only: [:show, :index] do
+    resources :reviews
+  end
   resources :roles
-  resources :reviews
   devise_for :users
-  
+
   namespace :admin do
     resources :users
     resources :people
