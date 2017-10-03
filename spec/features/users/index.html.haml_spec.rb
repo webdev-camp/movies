fb9a50abb521d5a2423296f0ffec4523066a446e
Luke Marco <luke.am89@hotmail.co.uk>
2017-10-03 16:48:49 +0300

    Added search and pagination to admin users

--- spec/features/users/index.html.haml_spec.rb
+++ spec/features/users/index.html.haml_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe "users/index" do
     user = @users.first
     expect(page).to have_link("Edit", href: edit_admin_user_path(user))
   end
+  it 'searches for a person by name' do
+    fill_in(:q_name_cont, with: 'john')
+    click_button('Search')
+    expect(page).to have_content('john')
+  end
+  it 'searches for a person by email' do
+    fill_in(:q_email_cont, with: 'example')
+    click_button('Search')
+    expect(page).to have_content('example')
+  end
+end
+
+describe "admin/users/index" do
+  before(:each) do
+    sign_admin_in
+    @users = create_list(:user, 100)
+    visit admin_users_path
+  end
+  it "only shows 25 movies at one time" do
+    list = page.all('a', :text => 'Show')
+    expect(list.length).to be(25)
+  end
 end
