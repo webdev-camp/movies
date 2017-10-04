f6edb472d3c8d6d942a8ab3171debf659de99d50
Luke Marco <luke.am89@hotmail.co.uk>
2017-10-04 16:10:49 +0300

    Added testing for recommendations page

---
+++ spec/features/recommendations/index.html.haml_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "recommendations/index" do
+  before (:each) do
+    recommendations_index_path
+  end
+
+  it 'goes to recommendation page' do
+  end
+
+end
