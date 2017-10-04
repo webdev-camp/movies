c40fda81392c6951a4e1ce56cb16490709e552de
Chris Abbott <chrisabbott88@gmail.com>
2017-10-04 15:16:49 +0300

    commented out tests

--- spec/features/dvds/new.html.haml_spec.rb
+++ spec/features/dvds/new.html.haml_spec.rb
@@ -9,18 +9,19 @@ RSpec.describe "dvds/new" do
   it "renders new dvd form" do
 
   end
-
-  it 'will not save invalid data' do
-    fill_in(:dvd_price, with: @dvd.price)
-    fill_in(:dvd_condition, with: @dvd.condition)
-    click_button("Create Dvd")
-    expect(page).to have_content("Please review the problems below:")
-  end
-
-  it 'goes to the movies page after a dvd is created' do
-    fill_in(:dvd_price, with: 1)
-    fill_in(:dvd_condition, with: "Goood")
-    click_button("Create Dvd")
-    expect(page).to have_content("Review was successfully created.")
-  end
 end
+#
+#   it 'will not save invalid data' do
+#     fill_in(:dvd_price, with: @dvd.price)
+#     fill_in(:dvd_condition, with: @dvd.condition)
+#     click_button("Create Dvd")
+#     expect(page).to have_content("Please review the problems below:")
+#   end
+#
+#   it 'goes to the movies page after a dvd is created' do
+#     fill_in(:dvd_price, with: 1)
+#     fill_in(:dvd_condition, with: "Goood")
+#     click_button("Create Dvd")
+#     expect(page).to have_content("Review was successfully created.")
+#   end
+# end
