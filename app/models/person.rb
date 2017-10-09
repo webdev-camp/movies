ff72dd2fcc9943c86fdec78d11447708dfa38c5c
Limaep <limae.phuah@gmail.com>
2017-10-09 11:46:14 +0300

    imdb tmdb link to right page & open in new window

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ApplicationRecord
   validates :name, :bio, presence: true
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" },
+  has_attached_file :picture, styles: { thumb: "100x150!",
+                                      medium: "300x450!",
+                                      large: "600X900!"},
                     default_url: "/images/person/picture/:style.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   validates :tmdb_id, numericality:{ only_integer: true, greater_than_or_equal_to: 0 }
