d0466e5ebdbb42df787659946d009fc05edf672a
Torsten Ruger <torsten@villataika.fi>
2017-10-12 12:29:15 +0300

    set up email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,24 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "movies_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        :address              => "smtp.yandex.com",
+        :port                 => 465,
+        :domain               => "moviefan.club",
+        :user_name            => "info@moviefan.club",
+        :password             => ENV["EMAIL_PASS"],
+        :ssl                  => true,
+        :tls                  => true,
+        :authentication       => :login,
+        :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = {  protocol: 'https',
+                                                host:     "moviefan.club" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {  from: "info@moviefan.club" ,
+                                            bcc:  "info@moviefan.club" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
