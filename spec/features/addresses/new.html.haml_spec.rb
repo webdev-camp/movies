c4fb0a8ee99cf2bce6e26e9739c62b1701029016
Chris Abbott <chrisabbott88@gmail.com>
2017-10-17 21:55:26 +0300

    Fixed address feature tests

--- spec/features/addresses/new.html.haml_spec.rb
+++ spec/features/addresses/new.html.haml_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "address/new" do
     visit new_address_path(@address)
     @address = create(:address, user: @user)
     @disc = create(:disc_for_sale, user: @user)
-
   end
 
   it 'loads new address input page' do
@@ -13,6 +12,15 @@ RSpec.describe "address/new" do
 
   it 'new address page has link to home' do
     expect(page).to have_link("Home", href: discs_path)
+  end
+
+  it 'goes to show addresses page when address is created' do
+    fill_in(:address_street, with: @address.street)
+    fill_in(:address_postcode, with: @address.postcode)
+    fill_in(:address_city, with: @address.city)
+    fill_in(:address_country, with: @address.country)
+    click_button("Create Address")
+    expect(page).to have_content(@address.street)
   end
 
 end
