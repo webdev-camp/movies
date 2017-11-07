21401423e7930fb37f943b93f05fecfcc143d7be
Limaep <limae.phuah@gmail.com>
2017-11-07 21:07:01 +0200

    get user_id into notification broadcast code

--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,6 +1,6 @@
 class NotificationChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "notification_channel"
+    stream_from "notification_channel_#{current_user.id}"
   end
 
   def unsubscribed
--- app/jobs/notification_broadcast_job.rb
+++ app/jobs/notification_broadcast_job.rb
@@ -3,7 +3,8 @@ class NotificationBroadcastJob < ApplicationJob
 
   def perform(notification_id)
     @notification = Notification.find(notification_id)
-    ActionCable.server.broadcast "notification_channel",
+    user_id = @notification.user_id
+    ActionCable.server.broadcast "notification_channel_#{user_id}",
                                         notification: html
   end
 
